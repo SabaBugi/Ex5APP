66ae1a2bca81b96488ad00c20a5b64c8da7b5370
საბა ბუგიანიშვილი <saba.boogy@gmail.com>
2025-05-06 19:34:59 +0400

    second commit

--- Ex1App/views.py
+++ Ex1App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import UserRegistrationForm, CategoryForm
 from django.contrib import messages
+from .models import UserRegistration
 
 def home_view(request):
     return render(request, 'Ex1App/home.html')
@@ -9,8 +10,19 @@ def user_registration_view(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.cleaned_data
+            user = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+
+            UserRegistration.objects.create(
+                username=user,
+                password=password,
+                email=email
+            )
+
             messages.success(request, 'User registered successfully!')
+
+            
     else:
         form = UserRegistrationForm()
     return render(request, 'Ex1App/user_registration.html', {'form': form})
